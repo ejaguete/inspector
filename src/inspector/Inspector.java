bcc89d7b17cb569aca17f6e2f546d54b232ccdb9
ericajoy.aguete <ericajoy.aguete@gfx01-3.cs.ucalgary.ca>
2017-10-18 12:28:41 -0600

    v0
    initial commit, empty class

---
+++ src/inspector/Inspector.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package inspector;
+
+/**
+ * @author ericajoy.aguete
+ *
+ */
+public class Inspector {
+	
+	public void inspect(Object o, boolean recurse) {
+		/*
+		 * TODO:
+		 * find the following info about the object:
+		 * - name of declaring class
+		 * - name of immediate superclass
+		 * - name of interfaces class implements
+		 * - methods class declares
+		 * 		- include:
+		 * 		- exceptions thrown
+		 * 		- parameter types
+		 * 		- return type
+		 * 		- modifiers
+		 * - constructors class declares
+		 * 		- include:
+		 * 		- parameter types
+		 * 		- modifiers
+		 * - fields class declares
+		 *  	- include:
+		 *  	- type
+		 *  	- modifiers
+		 *  	- current value of each field
+		 *  		* if field is an object reference and recursive is set to false, print "reference value" directly
+		 *  		(ref value = name of object's class, object's identity hash code)
+		 * - traverse inheritance hierarchy to find all methods, constructors, fields and field balues that each superclass/superinterface declares
+		 *  	- must handle arrays you might encounter
+		 *  	- print out name, component type, length, contents
+		 *  	
+		 */
+		
+		/* 
+		 * TODO:
+		 * recursive inspection :
+		 * if recurse = false, find info for object specified
+		 * if recurse = true, fully inspect each field that is an object (recurse into object)
+		 * 
+		 */
+		
+		
+	}
+
+}
