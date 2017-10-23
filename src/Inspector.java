e0bb894d57d804e71e74e467af1cb4d06532ab93
ejaguete <erica.aguete@gmail.com>
2017-10-23 16:18:50 -0600

    v1.5
    - moved long code to print out information to new method printInfo()

--- src/Inspector.java
+++ src/Inspector.java
@@ -4,6 +4,8 @@ import java.lang.reflect.*;
 
 public class Inspector {
 	private String format = "%-20s %-30s %n";
+	Object o;
+	boolean rec;
 	
 	public Inspector() {}
 	
@@ -16,131 +18,13 @@ public class Inspector {
 		out.println();	
 	}
 	
-	private void printInfo(Class c, Object o) {
-		
-	}
-	
-	private void inspectHierarchy(Class c, boolean recurse, Object o) {
-		if(!c.getName().equals("java.lang.Object") && 
-				!Modifier.isAbstract(c.getModifiers())) {		
-		
-			try {
-				Constructor superCons = c.getConstructor(null);
-				try {
-					Object superObj = superCons.newInstance(null);
-					inspect(superObj, recurse);
-				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-						| InvocationTargetException e1) {
-					e1.printStackTrace();
-				}
-			} catch (NoSuchMethodException | SecurityException e) {
-				e.printStackTrace();
-			}	
-		} else {
-			// manually print out info
-			out.println("--------\nSUPERCLASS SUMMARY:\n--------\n");
-			
-			Class[] supInterfaces = c.getInterfaces();
-			printList(supInterfaces, "INTERFACE");
-			
-			//find methods
-			Method[] methods = c.getDeclaredMethods();
-			printList(methods, "METHOD");
-			
-			
-			//find constructor
-			Constructor[] constructors = c.getConstructors();
-			printList(constructors, "CONSTRUCTOR");
-			
-			//find fields
-			
-
-			Field[] fields = c.getDeclaredFields();		
-			for (int i=0; i<fields.length;++i) {
-				//get field name
-				String info = fields[i] + " = ";
-				// read field
-				fields[i].setAccessible(true);
-
-				try {
-					Object value = fields[i].get(o);
-					
-					if(!fields[i].getType().isPrimitive()) 
-						info += value + " / hashcode=" + System.identityHashCode(value);
-					
-					else if(value!=null)
-						info += value;
-					
-					else
-						info += "null";
-					out.printf(format, "FIELD", info);
-					
-					if(fields[i].getType().isArray()) {
-						out.println("--------\nARRAY SUMMARY:\n--------\n");
-						
-						int len = Array.getLength(value);
-						out.printf(format, "ARRAY TYPE", fields[i].getType().getComponentType());
-						if(!fields[i].getType().getComponentType().toString().contains("java.io")) {
-							String contents = "";
-							for(int j=0; j<len-1;++j) {
-								contents += Array.get(value, j) + ", ";
-							}
-							contents += Array.get(value, len-1);
-							out.printf(format, "ARRAY CONTENTS", contents);
-							out.println("--------\nEND ARRAY SUMMARY\n--------\n");
-						} else {
-							out.printf(format, "ARRAY CONTENTS", "Cannot display array contents.");
-						}
-					} 
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			}	
-			out.println("--------\nEND SUPERCLASS SUMMARY\n--------\n");
-		}
-		
-	}
-
-	public void inspect(Object o, boolean recurse) {
-		/*
-		 * TODO:
-		 * find the following info about the object:
-		 * - name of declaring class
-		 * - name of immediate superclass
-		 * - name of interfaces class implements
-		 * - methods class declares
-		 * 		- include:
-		 * 		- exceptions thrown
-		 * 		- parameter types
-		 * 		- return type
-		 * 		- modifiers
-		 * - constructors class declares
-		 * 		- include:
-		 * 		- parameter types
-		 * 		- modifiers
-		 * - fields class declares
-		 *  	- include:
-		 *  	- type
-		 *  	- modifiers
-		 *  	- current value of each field
-		 *  		* if field is an object reference and recursive is set to false, print "reference value" directly
-		 *  		(ref value = name of object's class, object's identity hash code)
-		 * - traverse inheritance hierarchy to find all methods, constructors, fields and field values that each superclass/superinterface declares
-		 *  	- must handle arrays you might encounter
-		 *  	- print out name, component type, length, contents
-		 *  	
-		 */
-		
-		out.println("--------\nSUMMARY:\n--------\n");
-		
-		//find name of declaring class
-		Class c = o.getClass();
+	private void printInfo(Class c) {
 		out.printf(format, "CLASS", c.getName() + "\n");
 		
 		//find superclass
 		Class sup = c.getSuperclass();
 		out.printf(format, "SUPERCLASS", sup.getName() + "\n");
-		inspectHierarchy(sup, recurse, o);
+		inspectHierarchy(sup, rec, o);
 		
 		//find interfaces
 		Class[] interfaces = c.getInterfaces();
@@ -197,16 +81,68 @@ public class Inspector {
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				e.printStackTrace();
 			}
-			
+		}
+	}
+	
+	private void inspectHierarchy(Class c, boolean recurse, Object o) {
+		if(!c.getName().equals("java.lang.Object") && 
+				!Modifier.isAbstract(c.getModifiers())) {		
+		
+			try {
+				Constructor superCons = c.getConstructor(null);
+				try {
+					Object superObj = superCons.newInstance(null);
+					inspect(superObj, recurse);
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e1) {
+					e1.printStackTrace();
+				}
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}	
+		} else {
+			out.println("--------\nSUPERCLASS SUMMARY:\n--------\n");
+			printInfo(c);
+			out.println("--------\nEND SUPERCLASS SUMMARY\n--------\n");
 		}
 		
-		/* 
-		 * TODO:
-		 * recursive inspection :
-		 * if recurse = false, find info for object specified
-		 * if recurse = true, fully inspect each field that is an object (recurse into object)
-		 * 
-		 */	
 	}
 
+	public void inspect(Object object, boolean recurse) {
+		/*
+		 * TODO:
+		 * find the following info about the object:
+		 * - name of declaring class
+		 * - name of immediate superclass
+		 * - name of interfaces class implements
+		 * - methods class declares
+		 * 		- include:
+		 * 		- exceptions thrown
+		 * 		- parameter types
+		 * 		- return type
+		 * 		- modifiers
+		 * - constructors class declares
+		 * 		- include:
+		 * 		- parameter types
+		 * 		- modifiers
+		 * - fields class declares
+		 *  	- include:
+		 *  	- type
+		 *  	- modifiers
+		 *  	- current value of each field
+		 *  		* if field is an object reference and recursive is set to false, print "reference value" directly
+		 *  		(ref value = name of object's class, object's identity hash code)
+		 * - traverse inheritance hierarchy to find all methods, constructors, fields and field values that each superclass/superinterface declares
+		 *  	- must handle arrays you might encounter
+		 *  	- print out name, component type, length, contents
+		 *  	
+		 */
+		
+		this.o = object;
+		this.rec = recurse;
+		out.println("--------\nSUMMARY:\n--------\n");
+		printInfo(o.getClass());
+		
+	}
 }
+
