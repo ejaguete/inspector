c9f667390a93a02d2ff7c984b9c25ce74372a275
ejaguete <erica.aguete@gmail.com>
2017-10-23 16:11:42 -0600

    v 1.4
    - moved code to traverse hierarchy into inspectHierarchy()
    - able to read arrays now (cannot read java.io.* arrays)

--- src/Inspector.java
+++ src/Inspector.java
@@ -9,14 +9,95 @@ public class Inspector {
 	
 	private void printList(Object[] list, String header) {
 		if(list.length!=0) {
-			for (int i=0;i<list.length;++i)
-				out.printf(format, header, list[i]);
+			for (Object item : list)
+				out.printf(format, header, item);
 		} else
 			out.printf(format, header, "none");
 		out.println();	
 	}
 	
-	private void inspectHierarchy(Class c, boolean recurse) {
+	private void printInfo(Class c, Object o) {
+		
+	}
+	
+	private void inspectHierarchy(Class c, boolean recurse, Object o) {
+		if(!c.getName().equals("java.lang.Object") && 
+				!Modifier.isAbstract(c.getModifiers())) {		
+		
+			try {
+				Constructor superCons = c.getConstructor(null);
+				try {
+					Object superObj = superCons.newInstance(null);
+					inspect(superObj, recurse);
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e1) {
+					e1.printStackTrace();
+				}
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}	
+		} else {
+			// manually print out info
+			out.println("--------\nSUPERCLASS SUMMARY:\n--------\n");
+			
+			Class[] supInterfaces = c.getInterfaces();
+			printList(supInterfaces, "INTERFACE");
+			
+			//find methods
+			Method[] methods = c.getDeclaredMethods();
+			printList(methods, "METHOD");
+			
+			
+			//find constructor
+			Constructor[] constructors = c.getConstructors();
+			printList(constructors, "CONSTRUCTOR");
+			
+			//find fields
+			
+
+			Field[] fields = c.getDeclaredFields();		
+			for (int i=0; i<fields.length;++i) {
+				//get field name
+				String info = fields[i] + " = ";
+				// read field
+				fields[i].setAccessible(true);
+
+				try {
+					Object value = fields[i].get(o);
+					
+					if(!fields[i].getType().isPrimitive()) 
+						info += value + " / hashcode=" + System.identityHashCode(value);
+					
+					else if(value!=null)
+						info += value;
+					
+					else
+						info += "null";
+					out.printf(format, "FIELD", info);
+					
+					if(fields[i].getType().isArray()) {
+						out.println("--------\nARRAY SUMMARY:\n--------\n");
+						
+						int len = Array.getLength(value);
+						out.printf(format, "ARRAY TYPE", fields[i].getType().getComponentType());
+						if(!fields[i].getType().getComponentType().toString().contains("java.io")) {
+							String contents = "";
+							for(int j=0; j<len-1;++j) {
+								contents += Array.get(value, j) + ", ";
+							}
+							contents += Array.get(value, len-1);
+							out.printf(format, "ARRAY CONTENTS", contents);
+							out.println("--------\nEND ARRAY SUMMARY\n--------\n");
+						} else {
+							out.printf(format, "ARRAY CONTENTS", "Cannot display array contents.");
+						}
+					} 
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}	
+			out.println("--------\nEND SUPERCLASS SUMMARY\n--------\n");
+		}
 		
 	}
 
@@ -59,72 +140,7 @@ public class Inspector {
 		//find superclass
 		Class sup = c.getSuperclass();
 		out.printf(format, "SUPERCLASS", sup.getName() + "\n");
-		
-		if(!sup.getName().equals("java.lang.Object") && 
-				!Modifier.isAbstract(sup.getModifiers())) {
-			
-			Constructor superCons;
-			try {
-				superCons = sup.getConstructor(null);
-				try {
-					Object superObj = superCons.newInstance(null);
-					inspect(superObj, recurse);
-				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-						| InvocationTargetException e1) {
-					e1.printStackTrace();
-				}
-			} catch (NoSuchMethodException | SecurityException e) {
-				e.printStackTrace();
-			}	
-		} else {
-			// manually print out info
-			out.println("--------\nSUPERCLASS SUMMARY:\n--------\n");
-			
-			Class[] supInterfaces = sup.getInterfaces();
-			printList(supInterfaces, "INTERFACE");
-			
-			//find methods
-			Method[] methods = sup.getDeclaredMethods();
-			printList(methods, "METHOD");
-			
-			
-			//find constructor
-			Constructor[] constructors = sup.getConstructors();
-			printList(constructors, "CONSTRUCTOR");
-			
-			//find fields
-
-			Field[] fields = sup.getDeclaredFields();
-			if(fields.length!=0) {
-				for (int i=0; i<fields.length;++i) {
-					//get field name
-					String info = fields[i] + " = ";
-					// read field
-					fields[i].setAccessible(true);
-
-					try {
-						Object value = fields[i].get(o);
-						
-						if(!fields[i].getType().isPrimitive()) {
-							info += value + " / hashcode=" + System.identityHashCode(value);
-						}
-						else if(fields[i].getType().isArray()) {
-							
-						}
-						else if(value!=null)
-							info += value;
-						else
-							info += "null";
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						e.printStackTrace();
-					}
-					out.printf(format, "FIELD", info);
-				} 
-			} else
-				out.printf(format, "FIELD", "none");
-			
-			out.println("--------\nEND SUPERCLASS SUMMARY\n--------\n");
-		}
+		inspectHierarchy(sup, recurse, o);
 		
 		//find interfaces
 		Class[] interfaces = c.getInterfaces();
@@ -151,20 +167,37 @@ public class Inspector {
 			try {
 				Object value = fields[i].get(o);
 				
-				if(!fields[i].getType().isPrimitive()) {
+				if(!fields[i].getType().isPrimitive()) 
 					info += value + " / hashcode=" + System.identityHashCode(value);
-				}
-				else if(fields[i].getType().isArray()) {
-					
-				}
+				
 				else if(value!=null)
 					info += value;
+				
 				else
 					info += "null";
+				out.printf(format, "FIELD", info);
+				
+				if(fields[i].getType().isArray()) {
+					out.println("--------\nARRAY SUMMARY:\n--------\n");
+					
+					int len = Array.getLength(value);
+					out.printf(format, "ARRAY TYPE", fields[i].getType().getComponentType());
+					if(!fields[i].getType().getComponentType().toString().contains("java.io")) {
+						String contents = "";
+						for(int j=0; j<len-1;++j) {
+							contents += Array.get(value, j) + ", ";
+						}
+						contents += Array.get(value, len-1);
+						out.printf(format, "ARRAY CONTENTS", contents);
+						out.println("--------\nEND ARRAY SUMMARY\n--------\n");
+					} else {
+						out.printf(format, "ARRAY CONTENTS", "Cannot display contents of array.");
+					}
+				} 
 			} catch (IllegalArgumentException | IllegalAccessException e) {
 				e.printStackTrace();
 			}
-			out.printf(format, "FIELD", info);
+			
 		}
 		
 		/* 
